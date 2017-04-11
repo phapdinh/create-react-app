4ea5f938bc1d800adccdb31ee426f9150353efdf
Phap Dinh <centralparkloop@yahoo.com>
2017-04-11 09:39:42 -0400

    converted square into stateless component

--- components.js
+++ components.js
@@ -1,17 +1,9 @@
-class Square extends React.Component {
-   constructor() {
-     super();
-     this.state = {
-       value: null,
-     };
-  }
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
